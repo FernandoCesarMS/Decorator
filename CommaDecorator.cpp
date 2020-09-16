ca464fdca252e500f0bf984948e8c549bd11c934
Fernando Cesar <cmnando.silva@gmail.com>
2020-09-15 23:22:21 -0300

    CommaDecorator::_toString repaginado T.A

--- CommaDecorator.cpp
+++ CommaDecorator.cpp
@@ -7,16 +7,11 @@ std::string CommaDecorator::toString() {
     std::string a,b,c = "";
     std::istringstream ss(StreamDecorator::toString());
     std::getline(ss, a, ' ');
-    int i = 0;
     while (a != b){
         b = a;
         std::getline(ss, a, ' ');
         if (a!=b)
             c += b + " , ";
-        i++;
-        if (i > 10) {
-            break;
-        }
     }
     c += b;
   return c;
